1f05de2e329449631fe6e3fa3240564ce4c42f26
sitoi <133397418@qq.com>
2020-01-09 03:25:46 +0000

    Site updated: 2020-01-09 03:25:44

---
+++ libs/codeBlock/codeBlockFuction.js
@@ -0,0 +1,3 @@
+// build time:Thu Jan 09 2020 03:25:41 GMT+0000 (Coordinated Universal Time)
+$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
+//rebuild by neat 
---
+++ libs/codeBlock/codeShrink.js
@@ -0,0 +1,3 @@
+// build time:Thu Jan 09 2020 03:25:41 GMT+0000 (Coordinated Universal Time)
+$(function(){var e=$('<i class="fa fa-angle-up code-expand" aria-hidden="true"></i>');$(".code-area").prepend(e);$(".code-expand").on("click",function(){if($(this).parent().hasClass("code-closed")){$(this).siblings("pre").find("code").show();$(this).parent().removeClass("code-closed")}else{$(this).siblings("pre").find("code").hide();$(this).parent().addClass("code-closed")}})});
+//rebuild by neat 
